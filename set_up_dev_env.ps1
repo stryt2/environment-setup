62c8f4f54a3cb312466e7634561f4698d45fc81b
Ting-Chen Shang <ting-chen.shang@transmax.com.au>
2023-10-09 11:52:41 +1000

    Added dev env script.

---
+++ set_up_dev_env.ps1
@@ -0,0 +1,13 @@
+# Git
+choco install -y git
+choco install -y tortoisegit
+
+# IDE's
+choco install -y notepadplusplus
+choco install -y vscode
+
+# Operation
+choco install -y lens
+
+# Miscellaneous
+choco install -y 7zip
